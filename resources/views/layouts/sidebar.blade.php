9a572333a055b6b75c5e213f16cc996d801b6044
ahmedTarek14 <ahmedtarek.am@gmail.com>
2026-03-18 04:10:08 +0200

    side bar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,110 +1,106 @@
+@php
+    $isDashboard = request()->routeIs('admin.dashboard');
+    $isBanners = request()->routeIs('admin.banners.*');
+    $isSections = request()->routeIs('admin.sections.*');
+    $isSocial = request()->routeIs('admin.links.*');
+
+    $isServices = request()->routeIs('admin.services.*');
+    $isFaq = request()->routeIs('admin.faqs.*');
+    $isProjects = request()->routeIs('admin.projects.*');
+
+    $isSmartFeatures = request()->routeIs('admin.smart.features.*');
+    $isSmartBenefits = request()->routeIs('admin.smart.benefits.*');
+    $isSmart = $isSmartFeatures || $isSmartBenefits || request()->routeIs('smart.index');
+
+    $isSettingsGeneral = request()->routeIs('admin.settings.*');
+    $isSettingsLocations = request()->routeIs('admin.locations.*');
+    $isSettingsWorkHours = request()->routeIs('admin.work-hours.*');
+    $isSettings = $isSettingsGeneral || $isSettingsLocations || $isSettingsWorkHours;
+
+    // $isAboutUs = request()->is('admin/about-us/*') || request()->is('admin/about-us');
+@endphp
+
 <aside class="side-menu">
     <a href="{{ route('admin.dashboard') }}" class="logo">
         <img src="{{ aurl('images/logo.png') }}">
     </a>
     <ul>
-        <li class="{{ request()->is('admin') ? 'active' : '' }}">
+        <li class="{{ $isDashboard ? 'active' : '' }}">
             <a href="{{ route('admin.dashboard') }}">
                 <i class="fa fa-tachometer-alt"></i>
                 Dashboard
             </a>
         </li>
-        <li class="{{ request()->is('admin/banners') || request()->is('admin/banners/*') ? 'active' : '' }}">
+
+        <li class="{{ $isBanners ? 'active' : '' }}">
             <a href="{{ route('admin.banners.index') }}">
                 <i class="fa fa-image"></i>
                 Banners
             </a>
         </li>
 
-        <li class="{{ request()->is('admin/sections') || request()->is('admin/sections/*') ? 'active' : '' }}">
+        <li class="{{ $isSections ? 'active' : '' }}">
             <a href="{{ route('admin.sections.index') }}">
                 <i class="fa fa-list"></i>
                 Sections
             </a>
         </li>
 
-         <li class="{{ request()->is('admin/social-media') || request()->is('admin/social-media/*') ? 'active' : '' }}">
+        <li class="{{ $isSocial ? 'active' : '' }}">
             <a href="{{ route('admin.links.index') }}">
                 <i class="fa fa-share-alt"></i>
                 Social Media
             </a>
         </li>
-        <li class="sub-menu">
-            <a rel="noreferrer" href="javascript:void(0);">
-                <i class="fa fa-home"></i>
+
+        {{-- Pages Section --}}
+        <li class="sub-menu {{ $isServices || $isProjects || $isSmart ? 'active' : '' }}">
+            <a href="javascript:void(0);">
+                <i class="fa fa-file"></i>
                 Pages
                 <i class="fa fa-angle-down"></i>
             </a>
-            <ul style="display : {{ request()->is('admin/home/*') ? 'block' : '' }}">
-                <li
-                    class="{{ request()->is('admin/home/content') || request()->is('admin/home/content/*') ? 'active' : '' }}">
-                    {{-- <a href="{{ route('admin.banner.index') }}">
-                        - Static data
-                    </a> --}}
+            {{-- force display block for all Pages --}}
+            <ul style="display: {{ $isServices || $isProjects || $isSmart ? 'block' : '' }}">
+                <li class="{{ $isServices ? 'active' : '' }}">
+                    <a href="{{ route('admin.services.index') }}">- Services</a>
                 </li>
-                <li
-                    class="{{ request()->is('admin/home/features') || request()->is('admin/home/features/*') ? 'active' : '' }}">
-                    {{-- <a href="{{ route('admin.features.index') }}">
-                        - Features
-                    </a> --}}
-                </li>
-                <li
-                    class="{{ request()->is('admin/home/services') || request()->is('admin/home/services/*') ? 'active' : '' }}">
-                    {{-- <a href="{{ route('admin.services.index') }}">
-                        - Services
-                    </a> --}}
+                <li class="{{ $isProjects ? 'active' : '' }}">
+                    <a href="{{ route('admin.projects.index') }}">- Projects</a>
                 </li>
             </ul>
         </li>
-        <li class="sub-menu">
-            <a rel="noreferrer" href="javascript:void(0);">
-                <i class="fa fa-info"></i>
-                About us page
+
+        {{-- Settings Section --}}
+        <li class="sub-menu {{ $isSettings? 'active' : '' }}">
+            <a href="javascript:void(0);">
+                <i class="fa fa-cog"></i>
+                Settings
                 <i class="fa fa-angle-down"></i>
             </a>
-            <ul
-                style="display : {{ request()->is('admin/about-us/*') || request()->is('admin/about-us/*/edit') ? 'block' : '' }}">
-                <li
-                    class="{{ request()->is('admin/about-us') || request()->is('admin/about-us/*/edit') ? 'active' : '' }}">
-                    {{-- <a href="{{ route('admin.about.index') }}">
-                        - Content
-                    </a> --}}
+            <ul style="display: {{ $isSettings? 'block' : '' }}">
+                <li class="{{ $isSettingsGeneral ? 'active' : '' }}">
+                    <a href="{{ route('admin.settings.edit', ['setting' => 1]) }}">- General</a>
                 </li>
-                <li
-                    class="{{ request()->is('admin/about-us/members') || request()->is('admin/about-us/members/*') ? 'active' : '' }}">
-                    {{-- <a href="{{ route('admin.members.index') }}">
-                        - Members
-                    </a> --}}
+                <li class="{{ $isSettingsLocations ? 'active' : '' }}">
+                    <a href="{{ route('admin.locations.index') }}">- Locations</a>
                 </li>
+                <li class="{{ $isSettingsWorkHours ? 'active' : '' }}">
+                    <a href="{{ route('admin.work-hours.index') }}">- Work Hours</a>
+                </li>
+                {{-- <li class="{{ $isAboutUs ? 'active' : '' }}">
+                    <a href="{{ route('admin.about.index') }}">- About Us</a>
+                </li> --}}
             </ul>
         </li>
-        <li class="{{ request()->is('admin/portfolio/') || request()->is('admin/portfolio/*') ? 'active' : '' }}">
-            {{-- <a href="{{ route('admin.projects.index') }}">
-                <i class="fa fa-image"></i>
-                Portfolio 
-            </a>--}}
-        </li>
-        <li
-            class="sub-menu {{ request()->is('admin/messages/*') || request()->is('admin/messages') ? 'active' : '' }}">
-            <a rel="noreferrer" href="javascript:void(0);">
-                <i class="fa fa-inbox"></i>
-                Messages
-                <i class="fa fa-angle-down"></i>
+
+        {{-- Optional FAQ --}}
+        <li class="{{ $isFaq ? 'active' : '' }}">
+            <a href="{{ route('admin.faqs.index') }}">
+                <i class="fa fa-question-circle"></i>
+                FAQ
             </a>
-            <ul>
-                <li
-                    class="{{ request()->is('admin/messages/contact-us') || request()->is('admin/messages/contact-us/*') ? 'active' : '' }}">
-                    {{-- <a href="{{ route('admin.contact.index') }}">
-                        - Contact Us
-                    </a> --}}
-                </li>
-                <li
-                    class="{{ request()->is('admin/messages/service-request') || request()->is('admin/messages/service-request/*') ? 'active' : '' }}">
-                    {{-- <a href="{{ route('admin.service.index') }}">
-                        - Service Request
-                    </a> --}}
-                </li>
-            </ul>
         </li>
-    </ul><!--End Ul-->
+
+    </ul>
 </aside>
