574e6868d2da67bef9c786a64edf3fb3ab987756
ahmedTarek14 <ahmedtarek.am@gmail.com>
2026-04-16 01:58:36 +0200

    edit routes

--- Modules/Page/routes/api.php
+++ Modules/Page/routes/api.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use Modules\Page\Http\Controllers\Api\BannerController;
 
 
-Route::middleware(['app_language'])->prefix('sni')->group(function () {
-    Route::get('/home', [BannerController::class, 'homeBanner'])->name('sni.page.show');
-    Route::get('/about-sni', [BannerController::class, 'aboutBanner'])->name('sni.about.show');
-    Route::get('/contact-us', [BannerController::class, 'contactBanner'])->name('sni.contact.show');
-    Route::get('/services', [BannerController::class, 'servicesBanner'])->name('sni.services.show');
-    Route::get('/projects', [BannerController::class, 'projectsBanner'])->name('sni.projects.show');
-    Route::get('/development', [BannerController::class, 'developmentBanner'])->name('sni.development.show');
+Route::middleware(['app_language'])->prefix('sni/banner')->group(function () {
+    Route::get('/home', [BannerController::class, 'homeBanner'])->name('sni.banner.home.show');
+    Route::get('/about-sni', [BannerController::class, 'aboutBanner'])->name('sni.banner.about.show');
+    Route::get('/contact-us', [BannerController::class, 'contactBanner'])->name('sni.banner.contact.show');
+    Route::get('/services', [BannerController::class, 'servicesBanner'])->name('sni.banner.services.show');
+    Route::get('/projects', [BannerController::class, 'projectsBanner'])->name('sni.banner.projects.show');
+    Route::get('/development', [BannerController::class, 'developmentBanner'])->name('sni.banner.development.show');
 });
